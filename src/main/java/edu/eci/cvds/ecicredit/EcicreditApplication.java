56acf29369857c92b0a6d1fe925d59dfd5857d55
unknown <carlos.avellaneda-f@labinfo.is.escuelaing.edu.co>
2025-03-26 07:41:20 -0500

    Se inicia el spring

---
+++ src/main/java/edu/eci/cvds/ecicredit/EcicreditApplication.java
@@ -0,0 +1,13 @@
+package edu.eci.cvds.ecicredit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcicreditApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcicreditApplication.class, args);
+	}
+
+}
